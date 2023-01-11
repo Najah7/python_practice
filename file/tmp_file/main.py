a11b8a28b960e1d91bc7bafc0c9e1a1c238521bb
Najah7 <najahmatsuo7@gmail.com>
2023-01-11 23:02:24 +0900

    main関数の書き忘れ

--- file/tmp_file/main.py
+++ file/tmp_file/main.py
@@ -2,13 +2,17 @@ import tempfile
 
 # コンテクスマネージャーで勝手に削除してくれる
 
-# tmp file
-with tempfile.TemporaryFile(mode='w+') as tmp:
-    tmp.write('test')
-    tmp.seek(0)
-    print(tmp.read())
-    
-    
-# tmp dir
-with tempfile.TemporaryDirectory() as tmp_dir:
-    print(tmp_dir)
+def main():
+    # tmp file
+    with tempfile.TemporaryFile(mode='w+') as tmp:
+        tmp.write('test')
+        tmp.seek(0)
+        print(tmp.read())
+        
+        
+    # tmp dir
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        print(tmp_dir)
+        
+if __name__ == '__main__':
+    main()
--- time/main.py
+++ time/main.py
@@ -3,31 +3,35 @@ import time
 
 # NOTE:datetimeはログファイルの名前でよく使う。
 
-# get what time is now
-now = datetime.datetime.now()
+def main():
+    # get what time is now
+    now = datetime.datetime.now()
 
-today = datetime.date.today()
-
-
-# NOTE:formatについて詳しく：https://docs.python.org/ja/3/library/datetime.html
-print(now.isoformat())
-print(today)
-print(now.strftime('%Y年%m月%d日%H時%M分%S秒%fミリ秒'))
-
-# make datetime object
-time_obj = datetime.time(hour=6, minute=30, second=0)
-
-print(time_obj.isoformat())
-
-# calculate datetim object
-week = datetime.timedelta(weeks=1, days=3, hours=12, seconds=60, microseconds=1)
+    today = datetime.date.today()
 
 
-print(now - week)
+    # NOTE:formatについて詳しく：https://docs.python.org/ja/3/library/datetime.html
+    print(now.isoformat())
+    print(today)
+    print(now.strftime('%Y年%m月%d日%H時%M分%S秒%fミリ秒'))
 
-# take pause
-time.sleep(2)
-print('hello')
+    # make datetime object
+    time_obj = datetime.time(hour=6, minute=30, second=0)
 
-# get epoch seconds
-print(time.time())
+    print(time_obj.isoformat())
+
+    # calculate datetim object
+    week = datetime.timedelta(weeks=1, days=3, hours=12, seconds=60, microseconds=1)
+
+
+    print(now - week)
+
+    # take pause
+    time.sleep(2)
+    print('hello')
+
+    # get epoch seconds
+    print(time.time())
+    
+if __name__ == '__main__':
+    main()
