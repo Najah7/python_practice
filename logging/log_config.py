bd32bdc77037d281a23fc2e87cf4297868b834e0
Najah7 <najahmatsuo7@gmail.com>
2023-01-12 15:13:11 +0900

    main関数の書き忘れ

--- logging/log_config.py
+++ logging/log_config.py
@@ -4,44 +4,49 @@ import logging.config
 # わかりやすく分離できるのは良い点、pythonと違う形式になるのは考慮点
 # 個人的にはJSON形式に親しみがあるからdictがいいかも
 
-# You can chose to read config info from file(True) or dict(False)
-READ_FILE = True
+def main():
 
-if READ_FILE:
-    logging.config.fileConfig('logging.ini')
-else:
-    logging.config.dictConfig({
-        'version':1,
-        'formatters':{
-            'sampleFormatter':{
-                'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
-            }
-        },
-        'handlers': {
-            'sampleHandlers': {
-                'class': 'logging.StreamHandler',
-                'formatter': 'sampleFormatter',
-                'level': logging.DEBUG
-            }
-        },
-        'root': {
-            'handlers': ['sampleHandlers'],
-            'level': logging.WARNING,
-        },
-        'loggers': {
-            'simpleExample': {
+    # You can chose to read config info from file(True) or dict(False)
+    READ_FILE = True
+
+    if READ_FILE:
+        logging.config.fileConfig('logging.ini')
+    else:
+        logging.config.dictConfig({
+            'version':1,
+            'formatters':{
+                'sampleFormatter':{
+                    'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s'
+                }
+            },
+            'handlers': {
+                'sampleHandlers': {
+                    'class': 'logging.StreamHandler',
+                    'formatter': 'sampleFormatter',
+                    'level': logging.DEBUG
+                }
+            },
+            'root': {
                 'handlers': ['sampleHandlers'],
-                'level': logging.DEBUG,
-                'propagate': 0
+                'level': logging.WARNING,
+            },
+            'loggers': {
+                'simpleExample': {
+                    'handlers': ['sampleHandlers'],
+                    'level': logging.DEBUG,
+                    'propagate': 0
+                }
             }
-        }
-    })
+        })
 
-logger = logging.getLogger('simpleExample')
+    logger = logging.getLogger('simpleExample')
 
 
-logger.debug('debug')
-logger.info('info')
-logger.warning('warning')
-logger.error('error')
-logger.critical('critical')
+    logger.debug('debug')
+    logger.info('info')
+    logger.warning('warning')
+    logger.error('error')
+    logger.critical('critical')
+    
+if __name__ == '__main__':
+    main()
--- logging/logger.py
+++ logging/logger.py
@@ -1,41 +1,44 @@
 import logging
 
 # logger
-
 # NOTE:基本的に使うのはlogger。
 #       どこか一か所で基本的なloggingの設定をして
 #       その設定を使ってインスタンス化したloggerをそれぞれでカスタムして使うのが一般的
 
-# __name__を使うことが多い。
-# なぜなら違うファイルで呼び出したときにログにわかりやすく表示されるから
 
-# 歴史的な背景でlogger関係は関数でもキャメルケースを使うことが多い（違う会社が作ったらしい？？）
+def main():
+    # __name__を使うことが多い。
+    # なぜなら違うファイルで呼び出したときにログにわかりやすく表示されるから
 
-logger = logging.getLogger(__name__)
+    # 歴史的な背景でlogger関係の関数（ex)getLogger()）はキャメルケースを使うことが多い（違う会社が作ったらしい？？）
 
-logger.setLevel(logging.WARNING)
+    logger = logging.getLogger(__name__)
 
-# handlerの例
-# 下記はファイルに書きこむハンドラ。（ログのハンドラーはログが発生がイベント）
-# ハンドラを設定しない場合StreamHandlerが設定されているの思う（なぜなら標準出力に出力されるから）
-# ハンドラーの種類詳しくはhttps://docs.python.org/3/library/logging.handlers.html?highlight=logging%20handler#module-logging.handlers
-file_handler = logging.FileHandler('output2.log')
+    logger.setLevel(logging.WARNING)
 
-logger.addHandler(file_handler)
+    # handlerの例
+    # 下記はファイルに書きこむハンドラ。（ログのハンドラーはログが発生がイベント）
+    # ハンドラを設定しない場合StreamHandlerが設定されているの思う（なぜなら標準出力に出力されるから）
+    # ハンドラーの種類詳しくはhttps://docs.python.org/3/library/logging.handlers.html?highlight=logging%20handler#module-logging.handlers
+    file_handler = logging.FileHandler('output2.log')
 
-# filterの例
-class NoPassFilter(logging.Filter):
-    def filter(self, record):
-        # getMessage():ログのメッセージを取得するメソッド
-        message = record.getMessage()
-        return 'password' not in message
+    logger.addHandler(file_handler)
 
-# オブジェクト化して渡す
-logger.addFilter(NoPassFilter())
+    # filterの例
+    class NoPassFilter(logging.Filter):
+        def filter(self, record):
+            # getMessage():ログのメッセージを取得するメソッド
+            message = record.getMessage()
+            return 'password' not in message
 
-def output_log2():
-    logger.error('error from logger1')
-    logging.error('error from logger2')
-    # filterが適用されてるかのチェック
-    logger.error('error with password')
-    
+    # オブジェクト化して渡す
+    logger.addFilter(NoPassFilter())
+
+    def output_log2():
+        logger.error('error from logger1')
+        logging.error('error from logger2')
+        # filterが適用されてるかのチェック
+        logger.error('error with password')
+
+if __name__ == '__main__':
+    main()
