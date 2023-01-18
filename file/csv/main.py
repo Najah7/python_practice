f2fc2e3136fb271b51f87de058b162b49e453c26
Najah7 <najahmatsuo7@gmail.com>
2023-01-18 23:17:43 +0900

    ただフォーマッターを使ってみただけ

--- file/csv/main.py
+++ file/csv/main.py
@@ -1,31 +1,33 @@
-import csv 
+import csv
 
 # NOTE:windowの場合は改行が\r\nになり２行改行されるので、newline引数を指定したがいい
 
+
 def main():
-    
+
     print_message('書き込み')
-    
+
     with open('test.csv', 'w', newline='') as csv_f:
         field_names = ['Name', 'Number']
-        writer = csv.DictWriter(csv_f, fieldnames=field_names) 
+        writer = csv.DictWriter(csv_f, fieldnames=field_names)
         writer.writeheader()
-        
+
         names = ['taro', 'jiro', 'saburou']
-        
+
         for i in range(3):
             writer.writerow({'Number': i, 'Name': names[i]})
-            
+
     print_message('読み込み')
-    
+
     with open('test.csv', 'r', newline='') as csv_f:
         reader = csv.DictReader(csv_f)
         for row in reader:
             print(row['Number'], row['Name'])
-        
+
+
 def print_message(message):
-    print(f'===={message}====')  
-        
-        
+    print(f'===={message}====')
+
+
 if __name__ == '__main__':
     main()
